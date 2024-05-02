da16b037c2b7523dfec36123ca52907f4d7b2398
Hari Shankar Singh <166795950+harishankar4u@users.noreply.github.com>
2024-05-02 16:19:23 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrackingApi.Data.Entity;
+using TrackingApi.infrastructure.Query;
+using TrackingApi.infrastructure.Response;
+
+namespace TrackingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<EmployeeController> _logger;
+        public EmployeeController(IMediator mediator, ILogger<EmployeeController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<EmployeeResponse> get(int id)
+        {
+            var resp=await _mediator.Send(new GetbyidQuery(id));
+            return resp;
+        }
+    }
+}
---
+++ infrastructure/Query/EmployeeQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TrackingApi.Data.Entity;
+using TrackingApi.infrastructure.Response;
+
+namespace TrackingApi.infrastructure.Query
+{
+    public record GetbyidQuery(int id):IRequest<EmployeeResponse>;
+}
